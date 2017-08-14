d28d4c77c1abd1da02382bebdca0dd96b78aadbb
junjie020 <junjie020@163.com>
2017-08-14 17:34:06 +0800

    继续提升检测的精度

--- NumAnalyzerNative/NumanalyzerNative/LotteryDataAnalyzer.cpp
+++ NumAnalyzerNative/NumanalyzerNative/LotteryDataAnalyzer.cpp
@@ -279,43 +279,49 @@ ErrorType LotteryDataAnalyzer::ReadDataFromFile()
 	if (!iff)
 		return ErrorType::ET_FilePathIsNotValid;
 
-	std::string regMatchString;
+	std::wstring regMatchString(L"\t");
 
 	for (uint32 ii = 0; ii < 9; ++ii)
-		regMatchString += "\\d+\\s*,";
+		regMatchString += L"\\d+\\s*,?";
 
-	regMatchString += "\\d+";
+	regMatchString += L"\\d+";
 
-	std::regex regDataNum(regMatchString.c_str(), std::regex_constants::icase);
+	std::wregex regDataNum(regMatchString.c_str(), std::regex_constants::icase);
 
 	std::string line;
 	while (std::getline(iff, line))
 	{
-		//trim(line);
-		//const char tab = '\t';
+		/*
+		trim(line);
+		const char tab = '\t';
 
-		//std::vector<std::string> parts;
-		//boost::split(parts, line, boost::is_any_of("\t"));
+		std::vector<std::string> parts;
+		boost::split(parts, line, boost::is_any_of("\t"));
 
-		//if (parts.size() != 3)
-		//	return ErrorType::ET_FileFormatError;
+		if (parts.size() != 3)
+			return ErrorType::ET_FileFormatError;
 
-		//mLotteryData.push_back(LotteryLineData());
-		//LotteryLineData &lottery = mLotteryData.back();
-		//lottery.lotteryNum = std::stoi(parts[0]);
+		mLotteryData.push_back(LotteryLineData());
+		LotteryLineData &lottery = mLotteryData.back();
+		lottery.lotteryNum = std::stoi(parts[0]);
 
-		//lottery.date = parts[2];
+		lottery.date = parts[2];
 
-		//std::vector<std::string> dataParts;
-		//boost::split(dataParts, parts[1], boost::is_any_of(","));
+		std::vector<std::string> dataParts;
+		boost::split(dataParts, parts[1], boost::is_any_of(","));
+		*/
 
-		std::smatch matchResult;
-		if (std::regex_search(line, matchResult, regDataNum))
+		auto wLine = utf8_to_utf16(line);
+		line = utf16_to_utf8(wLine);
+
+		std::wsmatch matchResult;
+		if (std::regex_search(wLine, matchResult, regDataNum))
 		{
-			std::string strData = *matchResult.begin();
+			std::wstring strData = *matchResult.begin();
+			trim(strData);
 
 			std::vector<std::string> dataParts;
-			boost::split(dataParts, strData, boost::is_any_of(","));
+			boost::split(dataParts, strData, boost::is_any_of(", "));
 			if (dataParts.size() != 10)
 				return ErrorType::ET_FileFormatErrorWithWrongData;
 
